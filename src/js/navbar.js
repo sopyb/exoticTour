fba7f8537802aaa076e4fffd1aee6f84f9ec4141
Sopy <alexcristian44@yahoo.com>
2022-02-23 13:32:57 +0200

    fixed navbar not showing on refresh

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -2,12 +2,18 @@ let defaultNavbarOffset, navbar
 $(window).ready(() => {
     navbar = $("#navbar")
     defaultNavbarOffset = navbar.offset().top
+
+    if ($(window).scrollTop() > defaultNavbarOffset) {
+        navbar.addClass("fixToTop");
+    } else {
+        navbar.removeClass("fixToTop");
+    }
 })
 
 $(window).scroll(() => {
     var height = $(window).scrollTop();
 
-    if (height >= defaultNavbarOffset) {
+    if (height > defaultNavbarOffset) {
         navbar.addClass("fixToTop");
     } else {
         navbar.removeClass("fixToTop");
