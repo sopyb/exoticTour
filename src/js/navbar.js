cceac0454381c7e919ddc8a547f83fae8da854e7
Sopy <alexcristian44@yahoo.com>
2022-02-23 10:38:03 +0200

    navbar fix to top

---
+++ src/js/navbar.js
@@ -0,0 +1,19 @@
+let defaultNavbarOffset, navbar
+$(window).ready(() => {
+    navbar = $("#navbar")
+    defaultNavbarOffset = navbar.offset().top
+})
+
+$(window).scroll(() => {
+    var height = $(window).scrollTop();
+
+    if (height >= defaultNavbarOffset) {
+        navbar.addClass("fixToTop");
+    } else {
+        navbar.removeClass("fixToTop");
+    }
+
+    console.log(height)
+})
+
+console.log(navbar)
