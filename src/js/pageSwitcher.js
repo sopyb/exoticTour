1bcefd6397e8de9823b7996c222ba586596afb43
Sopy <alexcristian44@yahoo.com>
2022-03-03 08:41:03 +0200

    Setting height of second panel to fit content height

--- src/js/pageSwitcher.js
+++ src/js/pageSwitcher.js
@@ -1,18 +1,20 @@
-$(document).ready(function(){
+$(document).ready(function () {
     var anchor = window.location.hash.substring(1);
-
-    $("#content").load("src/html/" + getPage(anchor))
+    let content = $("#content")
+    content.load("src/html/" + getPage(anchor), () => {
+        content.parent().height(content.outerHeight(true))
+    })
 });
 
 function getPage(anchor) {
-    switch(anchor) {
+    switch (anchor) {
         case "about":
             return "aboutUs.html"
         case "contact":
             return "contact.html"
         default:
             return "home.html"
-            
+
     }
 }
 
@@ -35,9 +37,12 @@ function checkScroll() {
         enableScroll()
         window.removeEventListener('scroll', checkScroll);
 
-        $("#content").fadeOut(500, () => {
-            $("#content").load("src/html/" + getPage(location.href.split("#")[1]))
-            $("#content").fadeIn(500)
+        let content = $("#content")
+        content.fadeOut(500, async () => {
+            content.load("src/html/" + getPage(location.href.split("#")[1]), () => {
+                content.parent().height(content.outerHeight(true))
+            })
+            content.fadeIn(500)
         })
     }
 }
@@ -47,47 +52,56 @@ function checkScroll() {
 
 // left: 37, up: 38, right: 39, down: 40,
 // spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
-var keys = {37: 1, 38: 1, 39: 1, 40: 1};
+var keys = {
+    37: 1,
+    38: 1,
+    39: 1,
+    40: 1
+};
 
 function preventDefault(e) {
-  e.preventDefault();
+    e.preventDefault();
 
-  window.scrollTo({ 
-    top: defaultNavbarOffset,
-    behavior: "smooth"
-})
+    window.scrollTo({
+        top: defaultNavbarOffset,
+        behavior: "smooth"
+    })
 }
 
 function preventDefaultForScrollKeys(e) {
-  if (keys[e.keyCode]) {
-    preventDefault(e);
-    return false;
-  }
+    if (keys[e.keyCode]) {
+        preventDefault(e);
+        return false;
+    }
 }
 
 // modern Chrome requires { passive: false } when adding event
 var supportsPassive = false;
 try {
-  window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
-    get: function () { supportsPassive = true; } 
-  }));
-} catch(e) {}
+    window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
+        get: function () {
+            supportsPassive = true;
+        }
+    }));
+} catch (e) {}
 
-var wheelOpt = supportsPassive ? { passive: false } : false;
+var wheelOpt = supportsPassive ? {
+    passive: false
+} : false;
 var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
 
 // call this to Disable
 function disableScroll() {
-  window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
-  window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
-  window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
-  window.addEventListener('keydown', preventDefaultForScrollKeys, false);
+    window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
+    window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
+    window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
+    window.addEventListener('keydown', preventDefaultForScrollKeys, false);
 }
 
 // call this to Enable
 function enableScroll() {
-  window.removeEventListener('DOMMouseScroll', preventDefault, false);
-  window.removeEventListener(wheelEvent, preventDefault, wheelOpt); 
-  window.removeEventListener('touchmove', preventDefault, wheelOpt);
-  window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
+    window.removeEventListener('DOMMouseScroll', preventDefault, false);
+    window.removeEventListener(wheelEvent, preventDefault, wheelOpt);
+    window.removeEventListener('touchmove', preventDefault, wheelOpt);
+    window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
 }
